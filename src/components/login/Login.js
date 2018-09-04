f548da25b125b8c868b06bb00f8dd4eb3a72cc3b
Jeremiah <jeremiah3643@gmail.com>
2018-09-04 10:58:44 -0500

    login now functions

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+    state = {
+        loginEmail: "",
+        loginUsername: ""
+    }
+    handleFields = (event) => {
+        let values = {}
+        values[event.target.id] = event.target.value
+        this.setState(values)
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:8088/users?email=${this.state.loginEmail}&password=${this.state.loginUsername}`)
+            .then(r => r.json())
+            .then(user => {
+                if (user.length) {
 
+                    let userinfo = {
+                        userId: user[0].id
+                    }
+                    sessionStorage.setItem("userInfo", JSON.stringify(userinfo))
+                    alert("You have successfully logged in!")
+                }
+                else {
+                    alert("Email or Password do not match our records!")
+                }
+            })
+    }
 
     render() {
         return (
-        <div>
-            <label>Email</label>
-            <input></input>
-            <label>Username</label>
-            <input></input>
-            <button>Log In</button>
-        </div>
-    )
+            <div>
+                <label>Email</label>
+                <input onChange={this.handleFields} id="loginEmail"></input>
+                <label>Username</label>
+                <input onChange={this.handleFields} id="loginUsername"></input>
+                <button onClick={this.handleSubmit}>Log In</button>
+            </div>
+        )
     }
 
 
